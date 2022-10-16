343e36b5a3659d419e2e50187a0703ed9cc63c55
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-10-16 20:28:26 +0200

    Added training function pix2pix model

--- Utils/model_builder/tf_models/model_02.py
+++ Utils/model_builder/tf_models/model_02.py
@@ -1,23 +1,25 @@
 
-from ast import main
-from .. tensorflow_builder import tf_builder
+#from ..tensorflow_builder import tf_builder
 import tensorflow as tf
 from tensorflow import keras
 from keras.layers import Conv2D, Conv2DTranspose, LeakyReLU, BatchNormalizationV2, Dropout, Input, Concatenate, Lambda, ReLU
 from keras.activations import tanh
 from keras.models import Sequential
-
+from keras.optimizers import Adam
+from keras.losses import BinaryCrossentropy, MeanAbsoluteError
 
 class __model():
     """This model is a simple autoencoder as our base model"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,learning_rate=0.0002, *args, **kwargs):
         self.channels = 3
         self.strides = 1
         self.padding = "same"
         self.drop_rate = 0.50  # The droput rate in dropout layer
         self.de_num = 0
         self.en_num = 1
+        self.learning_rate = learning_rate
+        self._build_model()
 
     def _build_model(self):
         self.generator = self.generator()
@@ -26,17 +28,22 @@ class __model():
     def discrimnator(self):
         # C64-C128-C256-C512
         cnn_list = [128, 256, 512]
-        model = Sequential()
-        model.add(Conv2D(64, self.channels, strides=self.strides,
-                         padding=self.padding))
-        model.add(LeakyReLU(0.2))
+        input_x = Input(shape=[256, 256, 3], dtype=tf.float64)
+        input_y = Input(shape=[256, 256, 3], dtype=tf.float64)
+        x = Concatenate()([input_x,input_y])
+
+        x = Conv2D(64, self.channels*2, strides=2,
+                         padding=self.padding)(x)
+        x = LeakyReLU(0.2)(x)
+
         for cnn in cnn_list:
-            model.add(Conv2D(cnn, self.channels, strides=self.strides,
-                             padding=self.padding))
-            model.add(BatchNormalizationV2())
-            model.add(LeakyReLU(0.2))
-        model.add(Conv2D(1, self.channels, strides=self.strides,
-                         padding=self.padding, activation="sigmoid"))
+            x = Conv2D(cnn, self.channels, strides=self.strides,
+                             padding=self.padding)(x)
+            x = BatchNormalizationV2()(x)
+            x = LeakyReLU(0.2)(x)
+        output = Conv2D(1, self.channels, strides=self.strides,
+                         padding=self.padding, activation="sigmoid")(x)
+        model = keras.Model(inputs = (input_x,input_y),outputs =output )
         return model
 
     def en_block(self, input, cnn):
@@ -107,3 +114,52 @@ class __model():
 
         model = keras.Model(inputs=encoder.inputs, outputs=de)
         return model
+
+    @tf.function
+    def fit(self,dataset,epochs,l1_lambda=100):
+
+        generator = self.generator()
+        discriminator = self.discrimnator()
+
+        bce_loss = BinaryCrossentropy(from_logits=True)
+        L1_loss = MeanAbsoluteError()
+        disc_optim = Adam(learning_rate=self.learning_rate,beta_1=0.5,beta_2=0.999)
+        gen_optim = Adam(learning_rate=self.learning_rate,beta_1=0.5,beta_2=0.999)
+
+        for epoch in epochs:
+            for step,(x_batch_train,y_batch_train) in enumerate(dataset):
+                # training Generator first
+
+                discriminator.trainable=False # We don't want to train the discriminator here
+                with tf.GradientTape as tape:
+                    y_gen_fake = generator(x_batch_train,training=True)
+                    dis_pred_fake = discriminator((x_batch_train,y_gen_fake),training=False)
+                    gen_loss_fake = bce_loss(x_batch_train,y_gen_fake)
+                    l1 = L1_loss(y_gen_fake,y_batch_train) + l1_lambda
+                    gen_loss_val = gen_loss_fake + l1
+                
+                gen_grads = tape.gradient(gen_loss_val, generator.trainable_weights)
+                gen_optim.apply_gradients(zip(gen_grads, generator.trainable_weights))
+
+
+                # Training Discriminator
+                discriminator.trainable=True
+                with tf.GradientTape as tape:
+                    dis_pred_real = discriminator((x_batch_train,y_batch_train),training=True)
+                    dis_loss_real = bce_loss(dis_pred_real,tf.ones_like(dis_pred_real))
+
+                    dis_pred_fake = discriminator((x_batch_train,y_gen_fake),training=True)
+                    dis_loss_fake = bce_loss(dis_pred_fake,tf.zeros_like(dis_pred_real))
+                    dis_loss_val = (dis_loss_real+dis_loss_fake) / 2
+
+                dis_grads = tape.gradient(dis_loss_val, discriminator.trainable_weights)
+                disc_optim.apply_gradients(zip(dis_grads, discriminator.trainable_weights))
+
+                
+                
+
+
+            
+
+                
+
