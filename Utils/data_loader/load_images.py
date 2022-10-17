0718c57b1a3a7e5da13f8f38a30965d5810089c4
Ahmed Karem <ahmedkaremag@gmail.com>
2022-10-17 21:08:24 +0200

    Fixed issue with loading data and optimized it for better

--- Utils/data_loader/load_images.py
+++ Utils/data_loader/load_images.py
@@ -1,62 +1,75 @@
 import tensorflow as tf
 from Utils.config import load_config_file
 import os
-
+import glob
+import pandas as pd
 config_file = load_config_file()
 data_path = config_file["paths"]["data"]
 
+pic_shape = config_file["train_config"]["pic_shape"]
 
-def augment_images(x_image, y_image):
-    x = x_image
-    y = y_image
+def augment_images_brigh(x, y):
     x = tf.image.adjust_brightness(x, 0.1)
     y = tf.image.adjust_brightness(y, 0.1)
+    return x, y
+
+def augment_images_contrast(x, y):
     x = tf.image.adjust_contrast(x, 0.1)
     y = tf.image.adjust_contrast(y, 0.1)
+    return x, y
 
+def augment_images_flip(x, y):
     x = tf.image.random_flip_left_right(x)
     x = tf.image.random_flip_up_down(x)
     return x, y
 
+def produce_x_y_paths(data_version):
+    x_data = []
+    y_data = []
+    for folder in os.listdir(os.path.join(data_path, data_version))[:-1]: # -1 because we want to exclude test folder
+        path = os.path.join(data_path, data_version, folder)
+        origin_path = os.path.join(path, "origin")
+        origin_path = [path for path in glob.glob(origin_path+"/*.jpeg")][0] #It's one image so no problem
+        for i,image_path in enumerate(glob.glob(path+"/*.jpeg")):
+            x_data.append(image_path)
+            y_data.append(origin_path)
+    x_data = tf.data.Dataset.from_tensor_slices(x_data)
+    y_data = tf.data.Dataset.from_tensor_slices(y_data)
+    dataset = tf.data.Dataset.zip((x_data,y_data))
+    return dataset
+
+def load_x_y_images(data_version, add_augmentation=True,scale=False,resize=True):
+    dataset = produce_x_y_paths(data_version=data_version)
+    dataset = dataset.map(load_images)
+    print(dataset)
+    if add_augmentation:
+        dataset = dataset.map(augment_images_brigh)
+        #TODO Tensorflow has no proper function to append (combine two datasets together) # Concatenate doesn't work 
+        #augmented_set = augmented_set.concatenate(augmented_set.map(augment_images_contrast)) 
+        #augmented_set = augmented_set.concatenate(augmented_set.map(augment_images_flip))
+        #dataset = dataset.concatenate(augmented_set)
+    if scale:
+        dataset = dataset.map(scale_pic)
+    return dataset
+             
 
 def scale_pic(x_img, y_img):
     x_img = x_img / 255.
     y_img = y_img / 255.
     return x_img, y_img
 
+def load_images(x_path,y_path):
+    x_img = tf.io.read_file(x_path)
+    x_img = tf.io.decode_jpeg(x_img, channels=3)
+    x_img = tf.image.resize(x_img, pic_shape[:-1])
 
-def load_x_y_images(data_version, image_size=(344, 344), add_augmentation=True):
-    images = None
-    # -1 because we want to exclude test folder
-    for folder in os.listdir(os.path.join(data_path, data_version))[:-1]:
-        path = os.path.join(data_path, data_version, folder)
-        origin_path = os.path.join(path, "origin")
-        if not images:
-            img = tf.keras.utils.image_dataset_from_directory(
-                path, image_size=image_size, label_mode=None)
-            images = img
-            org_img = tf.keras.utils.image_dataset_from_directory(
-                origin_path, image_size=image_size, label_mode=None)
-        else:
-            img = tf.keras.utils.image_dataset_from_directory(
-                path, image_size=image_size, label_mode=None)
-            images = images.concatenate(img)
+    y_img = tf.io.read_file(y_path)
+    y_img = tf.io.decode_jpeg(y_img, channels=3)
+    y_img = tf.image.resize(y_img, pic_shape[:-1])
 
-            org_img.concatenate(tf.keras.utils.image_dataset_from_directory(
-                origin_path, image_size=image_size, label_mode=None))
-        for _ in img:
-            org_img = org_img.concatenate(org_img)
+    return x_img,y_img
 
-    dataset = tf.data.Dataset.zip((images, org_img))
-
-    if add_augmentation:
-        augmented_set = dataset.map(augment_images)
-        dataset = dataset.concatenate(augmented_set)
-
-    return dataset.map(scale_pic)
-
-
-def load_x_images(data_version, image_size=(344, 344), add_augmentation=True):
+def load_x_images(data_version, image_size=pic_shape[:-1], add_augmentation=True,scale=False):
     images = None
 
     for folder in os.listdir(os.path.join(data_path, data_version))[:-1]:
@@ -74,8 +87,9 @@ def load_x_images(data_version, image_size=(344, 344), add_augmentation=True):
     if add_augmentation:
         augmented_set = dataset.map(augment_images)
         dataset = dataset.concatenate(augmented_set)
-
-    return dataset.map(scale_pic)
+    if scale:
+        dataset = dataset.map(scale_pic)
+    return dataset 
 
 
 def load_test_image(img_path, pic_shape):
